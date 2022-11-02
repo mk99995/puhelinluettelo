50511fbbd9d2f69ab88d8c64e73a41cd9c574f0f
mk99995 <mejkorte@gmail.com>
2022-11-02 15:10:47 +0200

    post

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { response } = require("express");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     name: "bcvjdgfgdfdgdg",
@@ -42,7 +44,28 @@ app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
 
-  res.status(204).end();
+  res.status(404).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  let id = Math.floor(Math.random() * 99999);
+  console.log(req.headers);
+  const person = req.body;
+  if (
+    !persons
+      .map((person) => {
+        console.log(person.name);
+        person.name;
+      })
+      .includes(person.content.name)
+  ) {
+    person.content.id = id;
+    console.log(person.content.name);
+    console.log(person.content);
+    persons = [...persons, person.content];
+  } else {
+    console.log("asd");
+  }
 });
 
 const PORT = 3001;
